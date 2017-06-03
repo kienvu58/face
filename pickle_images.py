582c99ae13b9ef7eb2c57a60210a97c08ee29cfc
Kien Vu <vtk5995@gmail.com>
2017-06-03 16:06:50 +0700

    - pickle aligned images

--- pickle_images.py
+++ pickle_images.py
@@ -4,17 +4,25 @@ import pickle
 import os
 from utils import *
 from config import *
+from score import *
+
+
+def pickle_images(output_fn, align=False):
+    target = load(TARGET_SET)
+    images = []
+    for t in target:
+        t_img_path = os.path.join(FERET_DIR, *t)
+        img = cv2.imread(t_img_path, cv2.IMREAD_GRAYSCALE)
+
+        if align:
+            img = align_feret(img, *t)
+
+        images.append(img)
+        dump(output_fn, np.array(images), pickle.HIGHEST_PROTOCOL)
 
 
 def main():
-    target = load(TARGET_SET)
-    images = []
-    # for t in target:
-    #     t_img_path = os.path.join(FERET_DIR, *t)
-    #     img = cv2.imread(t_img_path, cv2.IMREAD_GRAYSCALE)
-    #     images.append(img)
-
-    dump("dat/target_images_1.dat", np.array(images), pickle.HIGHEST_PROTOCOL)
+    pickle_images("dat/aligned_target_images.dat", True)
 
 
 if __name__ == "__main__":
